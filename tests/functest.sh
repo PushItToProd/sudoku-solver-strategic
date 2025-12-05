d41925288e2676912c2c53c71218efb5d848f437
pushittoprod <31714132+PushItToProd@users.noreply.github.com>
2025-12-05 13:20:01 -0800

    (failing tests) test a second puzzle to thwart fakery

--- tests/functest.sh
+++ tests/functest.sh
@@ -79,24 +79,48 @@ main() {
   output="$(solve_sudoku "$puzzle_1_solved")"
   exit_code=$?
   if (( exit_code != 0 )); then
-    test::fail "Expected successful exit code (0) when a valid and solved puzzle is given"
+    test::fail "(puzzle 1) Expected successful exit code (0) when a valid and solved puzzle is given"
   fi
   if [[ "$output" != *"already solved"* ]]; then
-    test::fail "Expected output to contain 'already solved'"
+    test::fail "(puzzle 1) Expected output to contain 'already solved'"
   fi
 
   puzzle_1="$(read_puzzle 1)"
   output="$(solve_sudoku "$puzzle_1")"
   exit_code=$?
   if (( exit_code != 0 )); then
-    test::fail "Expected successful exit code (0) when a valid and solvable puzzle is given"
+    test::fail "(puzzle 1) Expected successful exit code (0) when a valid and solvable puzzle is given"
   fi
   if [[ "$output" == *"already solved"* ]]; then
-    test::fail "Expected output to not contain 'already solved'"
+    test::fail "(puzzle 1) Expected output to not contain 'already solved'"
   fi
 
   if [[ "$output" != *"$puzzle_1_solved"* ]]; then
-    test::fail "Expected output to contain the puzzle solution with zeroes replaced with solved digits"
+    test::fail "(puzzle 1) Expected output to contain the puzzle solution with zeroes replaced with solved digits"
+  fi
+
+  puzzle_2_solved="$(read_puzzle 2.solved)"
+  output="$(solve_sudoku "$puzzle_2_solved")"
+  exit_code=$?
+  if (( exit_code != 0 )); then
+    test::fail "(puzzle 2) Expected successful exit code (0) when a valid and solved puzzle is given"
+  fi
+  if [[ "$output" != *"already solved"* ]]; then
+    test::fail "(puzzle 2) Expected output to contain 'already solved'"
+  fi
+
+  puzzle_2="$(read_puzzle 2)"
+  output="$(solve_sudoku "$puzzle_2")"
+  exit_code=$?
+  if (( exit_code != 0 )); then
+    test::fail "(puzzle 2) Expected successful exit code (0) when a valid and solvable puzzle is given"
+  fi
+  if [[ "$output" == *"already solved"* ]]; then
+    test::fail "(puzzle 2) Expected output to not contain 'already solved'"
+  fi
+
+  if [[ "$output" != *"$puzzle_2_solved"* ]]; then
+    test::fail "(puzzle 2) Expected output to contain the puzzle solution with zeroes replaced with solved digits"
   fi
 
   test::check_result
